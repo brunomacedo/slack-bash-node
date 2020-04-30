9bb1c3b4f2376001fbf3276d2afbf3f4e16ea747
Bruno Macedo <contato@brunomacedo.com.br>
2020-04-30 16:47:18 -0300

    Update request.sh

--- request.sh
+++ request.sh
@@ -7,14 +7,10 @@ __SLACK_HOOK_URL="https://hooks.slack.com/services/..."
 __CARD_ICON="https://brunomacedo.com.br/public/images/favicon.png"
 __MSG_ERROR="Oops... We found an error on download file"
 __MSG_SUCCESS="Uhuu... File download completed successfully"
+__STATUS_CODE_API=""
 
 forceCreateFile() {
   mkdir -p "$(dirname "$1")" || return; touch "$1"
-}
-
-checkStatusCode() {
-  STATUS_CODE=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$1")
-  echo $STATUS_CODE
 }
 
 printCurrentTime() {
@@ -31,7 +27,7 @@ createSlackMessageBlock() {
           "block_id": "section567",
           "text": {
             "type": "mrkdwn",
-            "text": "*$1* \n*Location*: production \n*Response:* $(checkStatusCode $__API_REQUEST) \n*Service IP*: 100.0.0.560 \n*Pod*: ${HOSTNAME} \n<https://brunomacedo.com.br|View>"
+            "text": "*$1* \n*Location*: production \n*Response:*  $__STATUS_CODE_API \n*Service IP*: 100.0.0.560 \n*Pod*: ${HOSTNAME} \n<https://brunomacedo.com.br|View>"
           },
           "accessory": {
             "type": "image",
@@ -64,10 +60,22 @@ slack() {
 }
 
 request() {
+  rm $__DOWNLOAD_FILE 2>/dev/null
   forceCreateFile $__DOWNLOAD_FILE
-  curl -L -s "$__API_REQUEST" 2&>/dev/null > $__DOWNLOAD_FILE
 
-  __IS_EMPTY=$(cat $__DOWNLOAD_FILE)
+  __HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" $__API_REQUEST)
+  __HTTP_BODY=$(echo $__HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
+  __HTTP_STATUS=$(echo $__HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+  __STATUS_CODE_API="$__HTTP_STATUS"
+
+  if [ ! $__HTTP_STATUS -eq 200  ]; then
+    slack "$__SLACK_HOOK_URL" "$__MSG_ERROR"
+    return
+  fi
+
+  echo "$__HTTP_BODY" > $__DOWNLOAD_FILE
+
+  __IS_EMPTY=$(cat $__DOWNLOAD_FILE 2>/dev/null)
   if test -z "$__IS_EMPTY" || ! jq . $__DOWNLOAD_FILE &>/dev/null; then
     slack "$__SLACK_HOOK_URL" "$__MSG_ERROR"
     return
